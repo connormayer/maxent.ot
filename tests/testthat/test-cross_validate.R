55a6d58d7ca7c38610741c4503567126ee80cb97
Connor Mayer <cjmayer@uci.edu>
2022-11-08 20:50:18 -0800

    Added test for cross-validation

---
+++ tests/testthat/test-cross_validate.R
@@ -0,0 +1,26 @@
+test_that("Basic cross-validation calls", {
+  data_file <- system.file(
+    "extdata", "amp_demo_grammar_full.csv", package = "maxent.ot"
+  )
+
+  # Test call with scalar values
+  cv_results <- cross_validate(data_file, 10, 0, 1, in_sep=',')
+  expect_equal(nrow(cv_results), 1)
+
+  # Test call with vectors of scalars
+  cv_results <- cross_validate(data_file, 10, c(0, 1, 2), c(0.01, 0.1, 1), in_sep=',')
+  expect_equal(nrow(cv_results), 3)
+
+  # Test call with grid search
+  cv_results <- cross_validate(
+    data_file, 10, c(0, 1, 2), c(0.01, 0.1, 1), grid_search=TRUE, in_sep=','
+  )
+  expect_equal(nrow(cv_results), 9)
+
+  # Test call with list of vectors
+  cv_results <- cross_validate(
+    data_file, 10, list(c(0, 1, 2, 3), c(3, 4, 5, 6)),
+    list(c(0.01, 0.1, 1, 2), c(2, 1, 0.1, 0.01)), in_sep=','
+  )
+  expect_equal(nrow(cv_results), 2)
+})
