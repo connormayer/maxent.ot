bb446520566f281e904d935634ee86a6781f8d1c
Arapaimace <adarshv06.us@gmail.com>
2025-11-05 16:31:20 -0800

    some progress, still doesn't work

--- R/optimize.R
+++ R/optimize.R
@@ -289,31 +289,27 @@ objective_func <- function(constraint_weights, data, bias_params=NA) {
 calculate_probabilities <- function(constraint_weights, data,
                                     temperature = DEFAULT_TEMPERATURE,
                                     include_harmony = FALSE,
-                                    include_maxent_values = FALSE) {
+                                    include_maxent = FALSE) {
   freq_ix <- 2
-  harm_ix <- ncol(data) - 2
-  log_prob_ix <- harm_ix + 1
+  maxent_ix <- ncol(data) - 2
+  log_prob_ix <- maxent_ix + 1
 
-  harmony_values <- data[, (freq_ix + 1):(harm_ix - 1)] %*% matrix(constraint_weights)
-  maxent_values <- exp(-data[, harm_ix] / temperature)
-  data[, harm_ix] <- harmony_values
-  data[, log_prob_ix] <- log(apply(data, 1, normalize_row, data, harm_ix))
+  data[, maxent_ix] <- data[, (freq_ix + 1):(maxent_ix - 1)] %*% matrix(constraint_weights)
+  harmony_values <- data[, maxent_ix]
+  data[, maxent_ix] <- exp(-data[, maxent_ix] / temperature)
+  maxent_values <- data[, maxent_ix]
+  data[, log_prob_ix] <- log(apply(data, 1, normalize_row, data, maxent_ix))
 
-  insert_pos <- log_prob_ix - 1
   if (include_harmony) {
-    data$harmony <- harmony_values
-    cols <- names(data)
-    data <- data[, append(cols[cols != "harmony"], "harmony", after = insert_pos)]
-    insert_pos <- insert_pos - 1
+   data <- cbind(data, harmony_values)
   }
 
-  if (include_maxent_values) {
-    data$maxent_values <- maxent_values
-    cols <- names(data)
-    data <- data[, append(cols[cols != "maxent_values"], "maxent_values", after = insert_pos)]
+  if (include_maxent) {
+   data <- cbind(data, maxent_values)
   }
   return(data)
 }
+
 
 # Calculate gradients
 calculate_gradient <- function(constraint_weights,
