4b09d2b14932406b1fe2769fc9af109526d6942e
Arapaimace <adarshv06.us@gmail.com>
2026-02-24 00:59:41 -0800

    added input validation + better optimization function, need to make wrapper that uses optim + outputs)

--- R/bias.R
+++ R/bias.R
@@ -1,15 +1,41 @@
-optimization_function <- function(bias, training_data, test_data) {
+# validates mu and sigma according to passed data
+is_valid <- function(mu, sigma, train_data, test_data) {
 
-  model <- optimize_weights(training_data, mu = bias, sigma = bias)
+  # potentially unnecessary if assumed to only be used with valid splits
+  if (length(train_data) != length(test_data)){
+    stop("train and test data must be the same length")
+  }
+  if (!is.numeric(mu) || !is.numeric(sigma)) {
+    stop("mu and sigma must be numeric.")
+  }
 
-  train_ll <- model$loglik
-  weights  <- model$weights
+  if (length(mu) != length(sigma)) {
+    stop("mu and sigma must be the same length.")
+  }
 
-  predictions <- predict_probabilities(test_data, weights)
-  test_ll <- predictions$loglik
+  constraint_cols <- sapply(train_data, is.numeric)
+  n_constraints <- sum(constraint_cols) - 1
 
-  out <- list(bias = bias, train_ll = train_ll, test_ll = test_ll, weights = weights)
+  if (length(mu) > 1 && length(mu) != n_constraints) {
+    stop("If vectors, mu and sigma must match number of constraints.")
+  }
 
-  return(out)
+  return(TRUE)
 }
+
+
+#' Helper function that takes a bias vector, potential mu/sigma vector
+#' (if mapping to each value), train/test splits,
+#' outputs the test loglik to be used in optim later
+optimization_function <- function(mu, sigma, train_data, test_data) {
+
+  is_valid(mu, sigma, train_data)
+
+  model <- optimize_weights(train_data, mu = mu, sigma = sigma)
+
+  predictions <- predict_probabilities(test_data, model$weights)
+
+  return(predictions$loglik)
+}
+
 
