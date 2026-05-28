06d3b07db6cc82b0606d5524e3164ab90cfcc7b3
Arapaimace <adarshv06.us@gmail.com>
2026-05-28 13:28:36 -0700

    doesn't crash session, returns valid sigma

--- R/bias.R
+++ R/bias.R
@@ -1,25 +1,32 @@
 #' @export
 optimize_bias <- function(data = NULL, train_data = NULL, test_data = NULL,
                           k = NULL, sigma = 0.5, mu = 0,
-                          method = "L-BFGS-B", upper_bound = 100) {
+                          method = "L-BFGS-B", upper_bound = 100,
+                          control_params=NA) {
 
   if (!is.null(train_data) && !is.null(test_data)) {
     fn <- optimization_function
     fn_args <- list(train_data = train_data, test_data = test_data, mu = mu)
   }
 
-  result <- do.call(
-    stats::optim,
-    c(
-      list(
-        par = c(sigma),
-        fn = fn,
-        method = method,
-        lower = c(0),
-        upper = c(upper_bound)
-      ),
-      fn_args
-    )
+  if (!is.na(control_params)) {
+    control_params$fnscale <- -1
+  }
+  else {
+    control_params <- list(fnscale = -1)
+  }
+
+
+  result <- stats::optim(
+    par = c(sigma),
+    fn=fn,
+    train_data = train_data,
+    test_data = test_data,
+    mu=mu,
+    control=control_params,
+    # lower = c(0),
+    # upper = c(upper_bound),
+    # method="L-BFGS-B"
   )
 
   sigma_hat <- result$par[1]
@@ -35,34 +42,18 @@ optimize_bias <- function(data = NULL, train_data = NULL, test_data = NULL,
 
 optimization_function <- function(par, train_data, test_data, mu) {
 
-  val <- tryCatch(
-    {
+  sigma <- exp(par[1])
+  # sigma <- par[1]
+  # print(sigma) DEBUG
 
-      sigma <- exp(par[1])
-
-      ll <- optimize_fold(mu, sigma, train_data, test_data)
-
-      if (!is.finite(ll))
-        return(1e12)
-
-      return(-ll)
-    }, error = function(e) {
-        1e12
-      }
-    )
-
-  return(val)
-}
-
-
-
-optimize_fold <- function(mu, sigma, train_data, test_data) {
-
+  # print("Optimizing model") DEBUG
   model <- optimize_weights(train_data, mu = mu, sigma = sigma)
-
+  # model <- optimize_weights(train_data, mu = mu, sigma = sigma)
+  # print("Getting probabilities") DEBUG
   predictions <- predict_probabilities(test_data, model$weights)
-
+  # print("Got predictions") DEBUG
   ll <- predictions$loglik
+  # print(ll) DEBUG
 
   if (!is.finite(ll))
     return(1e12)
